c6cd037e7901b0917844afc3c8cc746a596b17d9
mucaho <mkucko@gmail.com>
2013-03-22 18:17:40 +0100

    Update ProgressBar.js

--- ProgressBar.js
+++ ProgressBar.js
@@ -6,19 +6,30 @@
 *
 * Used to display the progress of a specific task (e.g. progress of asset loading, HP bar). 
 * Internally it creates small 2D blocks representing a part of the progress spectrum. These blocks 
-* are either empty or filled with respect to the current progress.
+* are either empty or filled with respect to the current progress. Changes to the progress bar's 
+* 2D properties are reflected on to the blocks.
 *
 */
 Crafty.c("ProgressBar", {
 	init: function(entity) {
-		this._progressBarBlocks = {};
+		this.requires("2D");
+		this._progressBarBlocks = [];
+	},
+	_updateBlocks: function() {
+		var entity = this;
+		var blockWidth = entity.w / entity._progressBarBlocks.length;
+		
+		for (var i = 0; i < this._progressBarBlocks.length; i++) {
+			var block = this._progressBarBlocks[i];
+			block.attr({ x: entity.x + i*blockWidth, y: entity.y, 
+						w: blockWidth, h: entity.h, z: entity.z });
+		}
 	},
 	/**@
 	* #.progressBar
 	* @comp ProgressBar
 	* @sign public this .progressBar(String eventName, Number blockCount, Number maxValue, 
-	* String emptyColor, String filledColor, Number x, Number y, Number w, Number h, 
-	* Number z, String renderMethod )
+	* String emptyColor, String filledColor)
 	* @param eventName - String of the event to listen to. Trigger the event directly on the entity 
 	* the component gets applied to or trigger the event globally. The event object must be 
 	* a 0 <= Number <= maxValue representing the current progress.
@@ -27,33 +38,27 @@ Crafty.c("ProgressBar", {
 	* @param maxValue - The maximum value the incoming value can have.
 	* @param emptyColor - The color for 2D blocks that are empty.
 	* @param filledColor - The color for 2D blocks that are filled.
-	* @param x - X position of the progress bar.
-	* @param y - Y position of the progress bar.
-	* @param w - Width of the progress bar (each 2D block will have w/blockCount width).
-	* @param h - Height of the progress bar (each 2D block will have h height).
-	* @param z - Z position of the progress bar.
-	* @param renderMethod - "DOM" or "Canvas" are allowed.
+	* @param renderMethod - The render method for the blocks. "DOM" or "Canvas" are allowed.
 	* @return this - The current entity for chaining.
 	*
 	* Constructor method to setup the progress bar.
 	*
 	* @example
 	* ~~~
-	* Crafty.e("ProgressBar")
-	*	.progressBar("LOADING_PROGRESS", 10, 100, "black", "white", 150, 140, 100, 25, 0, "DOM");
+	* Crafty.e("2D, ProgressBar")
+	*	.attr({ x: 150, y : 140, w: 100, h: 25, z: 100 })
+	*	.progressBar("LOADING_PROGRESS", 10, 100, "black", "white", "DOM");
 	* ...
 	* Crafty.trigger("LOADING_PROGRESS", someValue);
 	* ~~~
 	*/
-	progressBar : function(eventName, blockCount, maxValue, emptyColor, filledColor, 
-							x, y, w, h, z, renderMethod) {
-		var blockWidth = w/blockCount;
-		var blockValue = maxValue/blockCount;
-		for (var i=0; i<blockCount; i++) {
-			this._progressBarBlocks[i] = 
+	progressBar : function(eventName, blockCount, maxValue, emptyColor, filledColor, renderMethod) {		
+		var blockValue = maxValue / blockCount;
+		
+		for (var i = 0; i < blockCount; i++) {
+			this._progressBarBlocks.push(
 				Crafty.e("2D, Color, " + renderMethod)
-					.attr({ x: x + i*blockWidth, y: y, w: blockWidth, h: h, z: z, 
-							_progressMin: blockValue*(i+1), 
+					.attr({ _progressMin: blockValue*(i+1), 
 							_emptyColor: emptyColor, _filledColor: filledColor})
 					.color(filledColor)
 					.bind(eventName, function(e) {
@@ -61,22 +66,29 @@ Crafty.c("ProgressBar", {
 							this.color(this._filledColor);
 						else
 							this.color(this._emptyColor);
-					});
+					}));
 		}
 		
+		this._updateBlocks();
 		
 		var progressCallback = function(event) {
-			for (var key in this._progressBarBlocks) {
-				if ( this._progressBarBlocks.hasOwnProperty(key) ) {
-					var block = this._progressBarBlocks[key];
-					block.trigger(eventName, event);
-				}
+			for (var i = 0; i < this._progressBarBlocks.length; i++) {
+				var block = this._progressBarBlocks[i];
+				block.trigger(eventName, event);
 			}
-		};
+		}
 		this.bind(eventName, progressCallback);
+		this.bind("Change", this._updateBlocks);
 		this.bind("RemoveComponent", function(component) {
 			if (component === "ProgressBar") {
 				this.unbind(eventName, progressCallback);
+				this.unbind("Change", this._updateBlocks);
+				
+				for (var i = 0; i < this._progressBarBlocks.length; i++) {
+					var block = this._progressBarBlocks[i];
+					block.destroy();
+				}
+				
 			}
 		});
 		
